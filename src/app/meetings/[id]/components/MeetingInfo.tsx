e9b15d7de60f2d9c6bf375272e616e74e42e8a95
victor herguedas <victor22junio@gmail.com>
2024-09-03 18:38:38 +0200

    feature: Handle when todos are loading

--- src/app/meetings/[id]/components/MeetingInfo.tsx
+++ src/app/meetings/[id]/components/MeetingInfo.tsx
@@ -1,4 +1,4 @@
-import { Meeting, Todo } from "@/domain/models/Meeting";
+import { Meeting, Todo, TodosState } from "@/domain/models/Meeting";
 import { Spinner, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import TodosComponent from "./Todos";
 import TranscriptionComponent from "./Transcription";
@@ -22,13 +22,24 @@ export default function MeetingInfo({ meeting, setMeeting }: Props) {
         saveTodos(todos, meeting.id)
     }
 
-    const todosState = todosError == null ?
-        (areTodosSaved && !areTodosLoading ?
-            "✅"
-            :
-            <div className="ml-2"><Spinner width={4} height={4}></Spinner></div>)
-        :
-        "❌"
+
+    let todosState: any = ""
+
+    if (meeting.todosState === TodosState.COMPLETED) {
+        if (todosError == null) {
+            if (areTodosSaved && !areTodosLoading) {
+                todosState = "✅"
+            } else {
+                todosState = <div className="ml-2"><Spinner width={4} height={4}></Spinner></div>
+            }
+        } else {
+            todosState = "❌"
+        }
+    } else if (meeting.todosState === TodosState.IN_PROGRESS || meeting.todosState === TodosState.WAITING) {
+        todosState = <div className="ml-2"><Spinner width={4} height={4}></Spinner></div>
+    } else if (meeting.todosState === TodosState.FAILED) {
+        todosState = "❌"
+    }
 
     return (
         <>
@@ -43,7 +54,21 @@ export default function MeetingInfo({ meeting, setMeeting }: Props) {
                         <TranscriptionComponent transcription={transcription} transcriptionState={transcriptionState} />
                     </TabPanel>
                     <TabPanel>
-                        <TodosComponent todos={todos} setTodos={setTodos} />
+                        {
+                            meeting.todosState === TodosState.COMPLETED &&
+                            <TodosComponent todos={todos} setTodos={setTodos} />
+                        }
+                        {
+                            (meeting.todosState === TodosState.IN_PROGRESS || meeting.todosState === TodosState.WAITING) &&
+                            <div className="flex flex-row">
+                                <div className="text-white">Loading todos...</div>
+                                <div className="ml-2"><Spinner width={4} height={4}></Spinner></div>
+                            </div>
+                        }
+                        {
+                            meeting.todosState === TodosState.FAILED &&
+                            <div className="text-white">Failed to load todos "❌"</div>
+                        }
                     </TabPanel>
                 </TabPanels>
             </Tabs>
--- src/app/meetings/[id]/page.tsx
+++ src/app/meetings/[id]/page.tsx
@@ -17,11 +17,14 @@ interface Props {
 }
 
 export default function MeetingPage({ params }: Props) {
+    const router = useRouter()
+
+    const goHome = () => {
+        router.push("/meetings")
+    }
     const { id } = params
     const { meeting, loading, error, fetchMeeting } = useGetMeetingRepository({ id })
     const [localMeeting, setLocalMeeting] = useState<Meeting | null>(null)
-
-
 
     useEffect(() => {
         fetchMeeting()
@@ -33,22 +36,24 @@ export default function MeetingPage({ params }: Props) {
         }
     }, [meeting])
 
-    if (localMeeting !== null) {
-        return meetingLoaded(localMeeting, setLocalMeeting)
+    if (localMeeting !== null && localMeeting !== undefined) {
+        return (
+            <div>
+                <ArrowBackIcon color="white" width="10" height="10" className="fixed top-0 left-0 mt-4 ml-4 text-white cursor-pointer" onClick={goHome}></ArrowBackIcon>
+                <MeetingLoaded meeting={localMeeting} setMeeting={setLocalMeeting} />
+            </div>
+        )
     }
 }
 
-const meetingLoaded = (meeting: Meeting, setMeeting: (meeting: Meeting) => void) => {
-    const router = useRouter()
+interface MeetingLoadedProps {
+    meeting: Meeting
+    setMeeting: (meeting: Meeting) => void
+}
 
-    const goHome = () => {
-        router.push("/meetings")
-    }
-
+const MeetingLoaded = ({meeting, setMeeting}: MeetingLoadedProps) => {
     return (
         <div className="text-white flex flex-col justify-center items-center w-full mt-6">
-            <ArrowBackIcon color="white" width="10" height="10" className="fixed top-0 left-0 mt-4 ml-4 text-white cursor-pointer" onClick={goHome}></ArrowBackIcon>
-
             <div className="w-3/4 sm:w-3/4 lg:w-3/5 xl:w-3/6">
                 <h1 className="text-5xl text-primary-200">{meeting.name}</h1>
                 <p className="text-sm mt-1">{getRelativeDateService(new Date(meeting.meetingDate))}</p>
